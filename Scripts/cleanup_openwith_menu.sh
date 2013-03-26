9ae77ff8bf01a9bbb08c9f4b45976d2bdb651dbf
Patrick <patrick@helmsdeep.at>
2013-03-26 08:27:40 +0100

    added clean openwith menu script

---
+++ Scripts/cleanup_openwith_menu.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#####################################################
+#                                                   #
+# clean up the open with context menu               # 
+#                                                   #
+# @version: 1.0                                     #
+# @date: 20.11.2012 14:19                           #
+#                                                   #
+#####################################################
+
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+killall Finder
